bc324d8f8787b23193c79cd92ca78c1120d8536e
ANSO1 <46899148+ANSO1@users.noreply.github.com>
2019-02-10 17:28:50 -0600

    Update main.js
    expresiónes regulares

--- web/main.js
+++ web/main.js
@@ -1,15 +1,10 @@
 
-function validate ()
- {
+function validate (){
   let userName = document.getElementById("username").value;
-  let userNameRGEX = /^[a-zA-Z0-9\.]+g/
+  let password1 = document.getElementById("password").value;
+  let userNameRGEX = /^[A-Z a-z 0-9\.]\S+[^`~!@#$%^&*()_|+\-=?;:'",<>\{\}\[\]\\\/]$/;
+  let let password1RGEX = /^[\w\.]\S{6,}$/;
   let userNameResult = userNameRGEX.test(userName);
-  alert("username:" + userNameResult);
+  let password1Result = password1RGEX.test(password);
+  alert("username:"+userNameResult + ", password: "+password1Result );
 };
-
-// // function validate () {
-// //   let password1 = document.getElementById("password").value;
-// //   let passwordRGEX = /[\w.\S]{6,}/
-// //   let password1Result = password1RGEX.test(password);
-// //   alert("password:" + passwordResult);
-// }
