213b89b1ef682680361413be1431badd062b5d6e
ANSO1 <46899148+ANSO1@users.noreply.github.com>
2019-02-10 20:19:58 -0600

    validación del usuario y contraseña
    la función ya es la correcta

--- web/main.js
+++ web/main.js
@@ -1,10 +1,21 @@
 
 function validate (){
   var userName = document.getElementById("username").value;
-  var password1 = document.getElementById("password").value;
+  var password = document.getElementById("pswd").value;
   var userNameRGEX = /^[A-Z a-z 0-9\.]\S+[^`~!@#$%^&*()_|+\-=?;:'",<>\{\}\[\]\\\/]$/;
-  var password1RGEX = /^[A-Z a-z 0-9\S]{6,}$/;
+  var passwordRGEX = /^[A-Z a-z 0-9\S]{6,}$/;
   var userNameResult = userNameRGEX.test(userName);
-  var password1Result = password1RGEX.test(password);
-  alert("username:"+userNameResult + ", password: "+password1Result);
+  var passwordResult = passwordRGEX.test(password);
+  alert("username:"+userNameResult + ", pswd: "+passwordResult);
+  if (userName == false)
+  {
+    alert("Por favor ingresa un nombre de usuario valido");
+    return false;
+  }
+  if(passwordResult == false)
+  {
+    alert("Por favor ingresa una contraseña valida");
+    return false;
+  }
+  return true;
 };
