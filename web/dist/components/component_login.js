415196ff4d5e945007775c1f1ad85bd7b723d20c
ErasmoCarlosMendoza <erasmo.mendoza@lakmisystems.com.mx>
2019-02-20 15:13:27 -0600

    Update component_login.js

--- web/dist/components/component_login.js
+++ web/dist/components/component_login.js
@@ -1,23 +1,58 @@
-new Vue({
-    el: "#component_login",
-    data: {
-        username: "",
-        password: "",
-    },
-    methods: {
-        login: function(){
-            axios({
-                method: 'post',
-                url: 'http://localhost/api/controllers/login.php',
-                data: {
-                  username: this.username,
-                  password: this.password,
-                }
-              })
-              .then(function (response) {
-                
-              })
-              .catch(error => console.log(error))
-        }
+let state = {
+  token: "",
+}
+
+let getters = {
+  getToken: state => state.token,
+}
+
+let mutations = {
+  CHANGE_TOKEN: (state, payload) => {
+    state.token = payload;
+  }
+}
+
+let actions = {
+  changeToken: (context, payload) => {
+    context.commit('CHANGE_TOKEN', payload);
+  }
+}
+
+let store = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
+})
+
+
+let component_login = new Vue({
+  el: "#component_login",
+  data: {
+    username: "",
+    password: "",
+  },
+  store: store,
+  computed: {
+    token() {
+      return this.$store.getters.getToken;
     }
+  },
+  methods: {
+    login: function () {
+      let store = this.$store;
+      axios({
+        method: 'post',
+        url: 'http://localhost/api/controllers/login.php',
+        data: {
+          username: this.username,
+          password: this.password,
+        }
+      })
+        .then(function (response) {
+          store.dispatch('changeToken', response.data.response);
+        })
+        .catch(error => console.log(error))
+    }
+  }
 })
