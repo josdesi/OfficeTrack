68663c88de9ab268cd16aa22bf657a7f3df8a892
ErasmoCarlosMendoza <erasmo.mendoza@lakmisystems.com.mx>
2019-02-25 12:58:24 -0600

    Actualiza environment.js

--- web/dist/config/environment.js
+++ web/dist/config/environment.js
@@ -1,9 +1,11 @@
 var env = {
 
-    /** Produccion **/
-
-    /** UAT **/
-
+   /** Produccion **/
+   // loginURL: 'http://localhost:9091',
+   // serverURL: 'http://localhost:9091',
+   /** UAT **/
+   // loginURL: 'http://localhost:9091',
+   // serverURL: 'http://localhost:9091',
     /** Local **/
    loginURL: 'http://localhost:9091',
    serverURL: 'http://localhost:9091',
